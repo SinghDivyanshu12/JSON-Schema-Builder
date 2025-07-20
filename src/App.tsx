f9ff21c39a5709f1aca17d9a61ab94c87c1db91d
SinghDivyanshu12 <112723454+SinghDivyanshu12@users.noreply.github.com>
2025-07-20 13:53:33 +0530

    Final Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+
+import { SchemaBuilder } from "./components/SchemaBuilder";
+
+function App() {
+  return <SchemaBuilder />;
+}
+
+export default App;
